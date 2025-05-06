862fc6758f483e7a615cfe64a3d44cc1844da551
Martin Stühmer <me@samtrion.net>
2025-05-06 22:28:10 +0200

    chore: Extended tests and constraints

---
+++ src/NetEvolve.FluentValue/TypeExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace NetEvolve.FluentValue;
+
+using System;
+
+internal static class TypeExtensions
+{
+    internal static object GetDefault(this Type value)
+    {
+        var underlying = Nullable.GetUnderlyingType(value);
+        if (underlying is not null)
+        {
+            return Activator.CreateInstance(underlying)!;
+        }
+        return Activator.CreateInstance(value)!;
+    }
+}
